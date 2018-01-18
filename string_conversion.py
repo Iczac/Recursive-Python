6495f6ef010cc13af2dad483e94b04c6bd7f1b02
Rog_Z <izdev.404@gmail.com>
2018-01-18 12:36:16 +0800

    File Name Changed

--- string_conversion.py
+++ string_conversion.py
@@ -1,6 +1,6 @@
 # Converting string value to Base Format Value
 
-def string_to_base_format(n,base):
+def string_conversion(n,base):
 
     if base == 16:
         convertString = "0123456789ABCDEF"
@@ -9,11 +9,11 @@ def string_to_base_format(n,base):
         if n < base:
             return convertString[n]
         else:
-            return string_to_base_format(n//base,base) + convertString[n%base]
+            return string_conversion(n//base,base) + convertString[n%base]
     else:
         if n < base:
             return n%2
         else:
-            return str(string_to_base_format(n//2,base)) + str(n%2)
+            return str(string_conversion(n//2,base)) + str(n%2)
 
-print('Converted value is ' + string_to_base_format(10,2))
+print('Converted value is ' + string_conversion(10,2))
